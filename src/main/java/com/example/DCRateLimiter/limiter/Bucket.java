af790dff9a9bf890760f44d654da14a41a6e38c2
Naman PC <namandeep444@gmail.com>
2025-12-31 11:51:24 +0530

    Initializing Bucket class

--- src/main/java/com/example/DCRateLimiter/limiter/Bucket.java
+++ src/main/java/com/example/DCRateLimiter/limiter/Bucket.java
@@ -1,4 +1,31 @@
 package com.example.DCRateLimiter.limiter;
 
 public class Bucket {
+    private double tokens;
+    private long lastRefillTimestamp;
+
+    public Bucket(double capacity){
+        this.tokens = capacity;
+        this.lastRefillTimestamp = System.nanoTime();
+    }
+
+    public double getTokens() {
+        return tokens;
+    }
+
+    public void setTokens(double tokens){
+        return tokens;
+    }
+
+    public void setTokens(double tokens){
+        this.tokens = tokens
+    }
+
+    public long getLastRefillTimestamp() {
+        return this.lastRefillTimestamp;
+    }
+
+    public void setLastRefillTimestamp(long lastRefillTimestamp){
+        this.lastRefillTimestamp = lastRefillTimestamp;
+    }
 }
--- src/main/java/com/example/DCRateLimiter/limiter/TokenBucketLimiter.java
+++ src/main/java/com/example/DCRateLimiter/limiter/TokenBucketLimiter.java
@@ -1,4 +1,46 @@
 package com.example.DCRateLimiter.limiter;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class TokenBucketLimiter {
+    private final long capacity;
+    private final double refillRatePerNanos;
+
+    private final Map<String, Bucket> buckets = new ConcurrentHashMap<>();
+
+    public TokenBucketLimiter(long capacity, long refillRatePerSecond) {
+        this.capacity = capacity;
+        this.refillRatePerNanos = refillRatePerSecond / 1_000_000_000.0;
+    }
+
+    public boolean allowRequest(String key){
+        Bucket bucket = buckets.computeIfAbsent(
+                key,
+                k-> new Bucket(capacity)
+        );
+
+        synchronized (bucket){
+            refill(bucket);
+
+            if(bucket.getTokens() >= 1){
+                bucket.setTokens(bucket.getTokens() - 1);
+                return true;
+            }
+            return false;
+        }
+    }
+
+    private void refill(Bucket bucket){
+        long now = System.nanoTime();
+        long elapsedNanos = now - bucket.getLastRefillTimestamp();
+
+        if (elapsedNanos <= 0 ) return;
+
+        double tokensToAdd = elapsedNanos * refillRatePerNanos;
+        double newTokenCount = Math.min(capacity, bucket.getTokens() + tokensToAdd);
+
+        bucket.setTokens(newTokenCount);
+        bucket.setLastRefillTimestamp(now);
+    }
 }
